d22b5f7630148b44d8a2e09e006364dd8fa2c4d7
jakob <jakob.oswald@gmx.net>
2021-12-13 12:59:32 +0100

    HHH-14973 Typo

--- src/test/java/org/hibernate/bugs/OptimisticLockingTestCase.java
+++ src/test/java/org/hibernate/bugs/OptimisticLockingTestCase.java
@@ -25,7 +25,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.persistence.Entity;
 import javax.persistence.OptimisticLockException;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,9 +42,9 @@ import static org.junit.Assert.assertTrue;
  * What's even better?  Fork hibernate-orm itself, add your test case directly to a module's unit tests, then
  * submit it as a PR!
  */
-public class OptimisticLockungTestCase extends BaseCoreFunctionalTestCase {
+public class OptimisticLockingTestCase extends BaseCoreFunctionalTestCase {
 
-	private static final Logger log = LoggerFactory.getLogger(OptimisticLockungTestCase.class);
+	private static final Logger log = LoggerFactory.getLogger(OptimisticLockingTestCase.class);
 
 	// Add your entities here.
 	@Override
